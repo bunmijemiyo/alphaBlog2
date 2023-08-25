6e5388728a861c1e97b7b2ef019a3730b7c4c2ea
olubunmi <bjemiyor@gmail.com>
2023-08-25 09:58:39 +0100

    AlphaBlog2 first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,11 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require rails-ujs
+//= require_tree .
+//= require popper
+
+
+import "@hotwired/turbo-rails"
+import "controllers"
+
+import "popper"
+import "bootstrap"
